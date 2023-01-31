db99de2f18dfe7cdf174051b252dde8007797fd9
shohei-mochizuki <121307266+shohei-mochizuki@users.noreply.github.com>
2023-01-31 14:06:20 -0500

    Added function of time calculation using dayjs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,9 @@
 // GLOBAL
+const latToronto = 43.651070; // Latitude of Toronto
+const lonToronto = -79.347015; // Longitude of Toronto
+const apiKeySeatGeek = "MzE3MDE3ODN8MTY3NTExMzI2My43OTE4ODI4" // API key for SeatGeek
+const range = "30mi"; // Default = 30miles (when not specified) // Feel free to change
+
 
 // 1-1. Create blank objects to store data 
 // key = eventID from SeatGeek
@@ -9,9 +14,17 @@ let savedData = {} // Saved (in Local Storage) data will be stored here
 
 // 1-2. Get today's date using DayJS
 // This is to specify dates to search events on coming weekend
-
+let today = dayjs();
+let dayOfWeek = today.format("d"); // Sun = 0, Mon = 1, Sat = 6
+let comingSat = today.add(6 - dayOfWeek,"day");
+let comingSun = comingSat.add(1,"day");
+let startDate = comingSat.format("YYYY-MM-DD");
+let endDate = comingSun.format("YYYY-MM-DD");
+console.log(startDate);
+console.log(endDate);
 
 // 1-3. Fetch data from SeatGeek
+
 
 
 // 1-4. Get saved (in Local Storage) data and put it in 'savedData'
