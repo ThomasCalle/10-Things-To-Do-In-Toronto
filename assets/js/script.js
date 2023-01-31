dcdbf6c500246e5a6d61364814a30302ed5b80ce
shohei-mochizuki <121307266+shohei-mochizuki@users.noreply.github.com>
2023-01-31 11:55:41 -0500

    Created initial JS file and added a flow as comments

---
+++ assets/js/script.js
@@ -0,0 +1,49 @@
+// GLOBAL
+
+// 1-1. Create blank objects to store data 
+// key = eventID from SeatGeek
+// value = [event category, event (short) name, event date/time, performer, venue name, venue address, venue latitude, venue longitude] 
+let fetchedData = {} // Fetched (new) data will be stored here 
+let savedData = {} // Saved (in Local Storage) data will be stored here
+
+
+// 1-2. Get today's date using DayJS
+// This is to specify dates to search events on coming weekend
+
+
+// 1-3. Fetch data from SeatGeek
+
+
+// 1-4. Get saved (in Local Storage) data and put it in 'savedData'
+
+
+// 2-1. Create lists & buttons for 'fetchedData' (lefthand side section)
+
+
+// 2-2. Create lists & buttons for 'savedData' (middle section)
+
+
+// Event Listeners
+// When a button of 'fetchedData' is clicked
+
+
+// When a button of 'savedData' is clicked
+
+
+// 3-1. Show data of the clicked event ('fetchedData') in the right-top section
+
+
+  // 3-1-1. Fetch map data from OpenStreetMaps and show it in the right-bottom section
+
+
+// 3-2. Show data of the clicked event ('savedData') in the right-top section
+
+
+  // 3-2-1. Fetch map data from OpenStreetMaps and show it in the right-bottom section
+
+
+
+
+
+
+
